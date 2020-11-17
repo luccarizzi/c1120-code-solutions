f0db8a53781af26a49a62bc33633ecb3afa3d868
Lucca Rizzi <luccarizzi29@gmail.com>
2020-11-17 17:43:32 -0600

    Complete javascript-functions-practice

---
+++ javascript-functions-practice/get-character-at-index.js
@@ -0,0 +1,6 @@
+/* exported getCharacterAtIndex */
+
+function getCharacterAtIndex(string, index) {
+  var characterAtIndex = string[index];
+  return characterAtIndex
+}
---
+++ javascript-functions-practice/get-first-initial-of-person.js
@@ -0,0 +1,6 @@
+/* exported getFirstInitialOfPerson */
+
+function getFirstInitialOfPerson(person) {
+  var firstInitialOfPerson = person.firstName[0];
+  return firstInitialOfPerson
+}
