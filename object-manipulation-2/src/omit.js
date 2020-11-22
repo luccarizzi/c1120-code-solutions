18e2a809d937158bc380bd6a2a778d0bcb4c4604
Lucca Rizzi <luccarizzi29@gmail.com>
2020-11-21 19:25:24 -0600

    Incomplete object-manipulation-2, on challenge 2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,16 @@
 /* exported omit */
+
+function omit(source, keys) {
+  // debugger;
+  var omittedObject = [];
+  for (var property in source) {
+    for (var i = 0; i < keys.length; )
+  }
+  return omittedObject;
+}
+
+omit({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']);
+
+omit({ qux: 4, corge: 5 }, ['bar', 'grault'])
+
+omit({}, ['foo', 'bar', 'baz'])
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,10 +2,12 @@
 
 function pick(source, keys) {
   var pickedObject = {};
-  for (var sourceKey in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (sourceKey === keys[i]) {
-        pickedObject[keys[i]] = source[keys[i]];
+  for (var value in source) {
+    if (source[value] !== undefined) {
+      for (var i = 0; i < keys.length; i++) {
+        if (value === keys[i]) {
+          pickedObject[keys[i]] = source[keys[i]];
+        }
       }
     }
   }
