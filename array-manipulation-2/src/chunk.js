15ecff11c61dcf511c9fb115aad13e6a20ceff29
Lucca Rizzi <luccarizzi29@gmail.com>
2020-11-22 21:41:42 -0600

    Complete array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,17 +1,19 @@
 /* exported chunk */
 
 function chunk(array, size) {
+  var finalArray = [];
   if (array.length === 0) {
     return [];
   } else {
-    var finalArray = [];
-    if (array.length % size === 0) {
-      for (var i = 0; i < array.length; i++) {
-        finalArray.push([array[i]]);
-      }
+    var newSize = size;
+    for (var i = 0; i < Math.floor(array.length / size); i++) {
+      var newArr = array.slice(i * size, newSize)
+      finalArray.push(newArr);
+      newSize += size;
+    }
+    if (array.length % size) {
+      finalArray.push([array[finalArray.length * size]]);
     }
   }
   return finalArray;
 }
-
-chunk(['foo', 'bar', 'baz', 'qux'], 2);
