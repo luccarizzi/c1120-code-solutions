84167c5f73b1ee998686a066fd1edcd6442689b9
Lucca Rizzi <luccarizzi29@gmail.com>
2020-11-25 23:05:56 -0600

    Incomplete object-manipulation2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,6 +1,7 @@
 /* exported defaults */
 
 function defaults(target, source) {
+  // debugger;
   if (Object.keys(target).length === 0) {
     for (var prop in source) {
       target[prop] = source[prop];
@@ -10,8 +11,10 @@ function defaults(target, source) {
       for (var propT in target) {
         if (propS !== propT) {
           target[propS] = source[propS];
+          console.log(target);
         } else {
           target[propT] = target[propT];
+          console.log(target);
         }
       }
     }
