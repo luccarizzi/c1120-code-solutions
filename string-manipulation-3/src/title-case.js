612562d730a6c395d4fb7713fcf2dd48f9c0bb3d
Lucca Rizzi <luccarizzi29@gmail.com>
2020-11-28 20:30:58 -0600

    Complete string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,15 @@
 /* exported titleCase */
+
+function titleCase(title) {
+
+  return
+}
+
+titleCase('composing software') // -> "Composing Software"
+titleCase('high performance browser networking') // -> "High Performance Browser Networking"
+titleCase('node.js in action') // -> "Node.js in Action"
+titleCase('professional JavaScript for web developers') // -> "Professional JavaScript for Web Developers"
+titleCase('secrets of the javascript ninja') // -> "Secrets of the JavaScript Ninja"
+titleCase('web audio api') // -> "Web Audio API"
+titleCase('javascript: the definitive guide') // -> "JavaScript: The Definitive Guide"
+titleCase('speaking Javascript: an in-depth guide for programmers') // -> "Speaking JavaScript: An In-Depth Guide for Programmers"
