fef569f10cc91f4272f715526fade5c7451b2131
Lucca Rizzi <luccarizzi29@gmail.com>
2020-11-29 22:45:00 -0600

    Complete string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,15 +1,55 @@
 /* exported titleCase */
 
 function titleCase(title) {
+  var separateWords = title.split(" ");
+  var finalString = "";
+  var space = " ";
 
-  return
+  for (var i = 0; i < separateWords.length; i++) {
+
+    if (separateWords[i].toLowerCase() === "javascript") {
+      finalString += "JavaScript" + space;
+    }
+
+    else if (separateWords[i].toLowerCase() === "javascript:") {
+      finalString += "JavaScript:" + space;
+    }
+
+    else if (separateWords[i].toLowerCase() === "api") {
+      finalString += "API" + space;
+    }
+
+    else if (separateWords[i].toLowerCase() === "web") {
+      finalString += "Web" + space;
+    }
+
+    else {
+
+      if (separateWords[i].length >= 4) {
+        finalString += separateWords[i][0].toUpperCase();
+        for (var k = 1; k < separateWords[i].length; k++) {
+          if (separateWords[i][k - 1] === "-") {
+            finalString += separateWords[i][k].toUpperCase();
+          } else {
+            finalString += separateWords[i][k].toLowerCase();
+          }
+        }
+        finalString += space;
+      }
+
+      else if (separateWords[i - 1][separateWords[i - 1].length - 1] === ":") {
+        finalString += separateWords[i][0].toUpperCase();
+        for (var j = 1; j < separateWords[i].length; j++) {
+          finalString += separateWords[i][j].toLowerCase();
+        }
+        finalString += space;
+      }
+
+      else if (separateWords[i].length <= 3) {
+        finalString += separateWords[i].toLowerCase() + space;
+      }
+
+    }
+  }
+  return finalString.trimEnd();
 }
-
-titleCase('composing software') // -> "Composing Software"
-titleCase('high performance browser networking') // -> "High Performance Browser Networking"
-titleCase('node.js in action') // -> "Node.js in Action"
-titleCase('professional JavaScript for web developers') // -> "Professional JavaScript for Web Developers"
-titleCase('secrets of the javascript ninja') // -> "Secrets of the JavaScript Ninja"
-titleCase('web audio api') // -> "Web Audio API"
-titleCase('javascript: the definitive guide') // -> "JavaScript: The Definitive Guide"
-titleCase('speaking Javascript: an in-depth guide for programmers') // -> "Speaking JavaScript: An In-Depth Guide for Programmers"
