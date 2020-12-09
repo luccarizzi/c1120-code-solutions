203e019908f598c7629c580643f8ce468e0ef2be
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-09 17:11:10 -0600

    Complete 3. zip(first, second)

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,15 @@
 /* exported zip */
+
+function zip(first, second) {
+  var zipped = [];
+  if (first.length <= second.length) {
+    for (var i = 0; i < first.length; i++) {
+      zipped.push([first[i], second[i]]);
+    }
+  } else {
+    for (var i = 0; i < second.length; i++) {
+      zipped.push([first[i], second[i]]);
+    }
+  }
+  return zipped;
+}
