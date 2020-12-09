9bdd059497c2bc36610b584a9b754a4cddc446f7
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-08 21:45:56 -0600

    Complete 1. flatten(array).

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,15 @@
 /* exported flatten */
+
+function flatten(array) {
+  var flatArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] === "object") {
+      for (var j = 0; j < array[i].length; j++) {
+        flatArray.push(array[i][j]);
+      }
+    } else {
+      flatArray.push(array[i]);
+    }
+  }
+  return flatArray;
+}
