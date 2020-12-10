b5a2224aeca07f4cead2b05cad1f64bcf4734af0
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-09 18:55:37 -0600

    No progress.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,14 @@
 /* exported unique */
+
+function unique(array) {
+
+}
+
+unique(['foo', 'bar', 'baz', 'qux', 'bar', 'qux', 'corge', 'baz']);
+// -> ["foo", "bar", "baz", "qux", "corge"]
+unique([1, 3, 3, 2, 5, 2, 1, 4, 5, 5]);
+// -> [1, 3, 2, 5, 4]
+unique([false, true, false, true]);
+// -> [false, true]
+unique([]);
+// -> []
