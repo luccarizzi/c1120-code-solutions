337f574c05b75b989ef1239e5335dab57efd30a2
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-10 15:25:19 -0600

    Clicking on each dot activates the move function.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -16,33 +16,32 @@ $iList[0].setAttribute("class", "fas fa-circle");
 function carousel() {
   if (count === 0) {
     $carouselImage.setAttribute("src", imagesSrc[count]);
-    console.log(count);
+    // console.log(count);
     $iList[0].setAttribute("class", "fas fa-circle");
     $iList[4].setAttribute("class", "far fa-circle");
     count++;
   } else if (count < 4) {
     $carouselImage.setAttribute("src", imagesSrc[count]);
-    console.log(count);
+    // console.log(count);
     $iList[count].setAttribute("class", "fas fa-circle");
     $iList[count - 1].setAttribute("class", "far fa-circle");
     count++;
   } else {
     $carouselImage.setAttribute("src", imagesSrc[count]);
-    console.log(count);
-    console.log("Lap")
+    // console.log(count);
+    // console.log("Lap")
     $iList[4].setAttribute("class", "fas fa-circle");
     $iList[count - 1].setAttribute("class", "far fa-circle");
     count = 0;
   }
 }
 
-timer = setInterval(carousel, 1000);
+timer = setInterval(carousel, 500);
 
 function move(moveCount) {
+  // console.log(count);
   clearInterval(timer);
-  console.log("count is on:", count);
   if (count === 0) {
-    console.log("count is on 0.")
     $iList[4].setAttribute("class", "far fa-circle");
   } else {
     $iList[count - 1].setAttribute("class", "far fa-circle");
@@ -50,5 +49,14 @@ function move(moveCount) {
   $iList[moveCount].setAttribute("class", "fas fa-circle");
   $carouselImage.setAttribute("src", imagesSrc[moveCount]);
   count = moveCount;
-  timer = setInterval(carousel, 1000);
+  timer = setInterval(carousel, 500);
 }
+
+var $dots = document.querySelector(".dots");
+
+$dots.addEventListener("click", function(event) {
+  if (event.target.nodeName === "I") {
+    var moveCount = Number(event.target.id)
+    move(moveCount);
+  }
+})
