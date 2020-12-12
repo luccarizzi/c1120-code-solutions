e6999908484144399778e8175f9d6ae89d44e90c
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-12 15:02:29 -0600

    Complete 7. union(first, second).

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,16 @@
 /* exported union */
+
+function union(first, second) {
+  var unionArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (!unionArray.includes(first[i])) {
+      unionArray.push(first[i])
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (!unionArray.includes(second[j])) {
+      unionArray.push(second[j])
+    }
+  }
+  return unionArray;
+}
