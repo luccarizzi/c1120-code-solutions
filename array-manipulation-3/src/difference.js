c2a79fcfc9e2fde62c1513d7e6d5ef2ca28463ab
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-12 14:33:55 -0600

    Complete 4. unique(array).

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,13 @@
 /* exported difference */
+
+function difference(first, second) {
+
+}
+
+difference([2, 1], [2, 3]);
+// -> [1, 3]
+difference(['html', 'css', 'javascript'], ['php', 'css', 'sql']);
+// -> ["html", "javascript", "php", "sql"]
+difference(
+  ['a', 'link', 'to', 'the', 'past'],['the', 'adventure', 'of', 'link'])
+// -> ["a", "to", "past", "adventure", "of"]
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,14 +1,11 @@
 /* exported unique */
 
 function unique(array) {
-
+  var uniqueArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (!uniqueArray.includes(array[i])) {
+      uniqueArray.push(array[i]);
+    }
+  }
+  return uniqueArray;
 }
-
-unique(['foo', 'bar', 'baz', 'qux', 'bar', 'qux', 'corge', 'baz']);
-// -> ["foo", "bar", "baz", "qux", "corge"]
-unique([1, 3, 3, 2, 5, 2, 1, 4, 5, 5]);
-// -> [1, 3, 2, 5, 4]
-unique([false, true, false, true]);
-// -> [false, true]
-unique([]);
-// -> []
