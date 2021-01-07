54f01ceb2404648c97aaebf1a8bfbffe5548d7ac
Lucca Rizzi <luccarizzi29@gmail.com>
2021-01-07 20:34:21 +0000

    Complete express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+let nextId = 1;
+
+const grades = {
+
+}
+
+app.get('/api/grades', (req, res) => {
+  let gradesArray = [];
+  for (let id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
+})
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  req.body['id'] = nextId;
+  grades[nextId] = req.body;
+  nextId++;
+  res.status(201).send(req.body)
+})
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000...')
+})
