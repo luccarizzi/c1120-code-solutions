97473e33c7a89533be2249375d4b16da3d948090
Lucca Rizzi <luccarizzi29@gmail.com>
2021-01-08 22:58:15 +0000

    In progress express-notes-api. On operation DELETE, step 2.

---
+++ express-notes-api/index.js
@@ -0,0 +1,77 @@
+const dataJson = require('./data.json');
+const notes = dataJson.notes;
+
+const express = require('express');
+const app = express();
+
+const fs = require('fs');
+
+app.use(express.json());
+
+app.get('/api/notes', (req, res) => {
+  let notesArray = [];
+  for (let id in notes) {
+    notesArray.push(notes[id]);
+  }
+  res.json(notesArray);
+})
+
+app.get('/api/notes/:id', checkIntSign, (req, res) => {
+  const id = parseInt(req.params.id);
+  for (let note in notes) {
+    if (parseInt(note) === id) {
+      res.status(200).json(notes[id]);
+      return;
+    }
+  }
+  res.status(404).json({ 'error': `cannot find note with id ${id}`});
+})
+
+function checkIntSign(req, res, next) {
+  if (Math.sign(req.params.id) === -1) {
+    res.status(400).json({ 'error': 'id must be a positive integer' });
+    return;
+  } else {
+    next()
+  }
+}
+
+app.post('/api/notes', checkContent, (req, res) => {
+  req.body['id'] = dataJson.nextId;
+  notes[dataJson.nextId] = req.body;
+  const newData = JSON.stringify(dataJson, null, 2);
+
+  fs.writeFile('./data.json', newData, 'utf8', err => {
+    if (err) {
+      res.status(500).json({ "error": "An unexpected error occurred." });
+    } else {
+      res.status(201).json(req.body);
+    }
+  })
+  dataJson.nextId++;
+})
+
+function checkContent(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ 'error': 'content is a required field' });
+    return;
+  } else {
+    next();
+  }
+}
+
+app.delete('/api/notes/:id', checkIntSign, checkId, (req, res) => {
+  res.send(req.params.id)
+})
+
+function checkId (req, res, next) {
+  const id = parseInt(req.params.id);
+  for (let note in notes) {
+    if (parseInt(note) === id) {
+      return next();
+    }
+  }
+  return res.status(404).json({ 'error': `cannot find note with id ${id}` });
+}
+
+app.listen(3000);
