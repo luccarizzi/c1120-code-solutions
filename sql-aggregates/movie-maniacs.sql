12fddad92a3f9fa44f1b660581812158ae1ec3b7
Lucca Rizzi <luccarizzi29@gmail.com>
2021-01-13 18:03:45 +0000

    Complete sql-aggregates

--- sql-aggregates/movie-maniacs.sql
+++ sql-aggregates/movie-maniacs.sql
@@ -1,10 +1,6 @@
-select  "c"."firstName" as "First Name",
-        "c"."lastName" as "Last Name",
-        "p". as "payment"
-
-
-from    "customers" as "c"
-
--- join    "rentals" as "r" using ("customerId")
-
-join    "payments" as "p" using ("customerId")
+select    "c"."firstName" as "First Name",
+          "c"."lastName" as "Last Name",
+          sum("p"."amount") as "payment"
+from      "payments" as "p"
+join       "customers" as "c" using ("customerId")
+group by  "firstName", "lastName"
