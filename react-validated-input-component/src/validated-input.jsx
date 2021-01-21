3a1a2d05f3ea3b6974fae90fb5c3583b8646ec63
Lucca Rizzi <luccarizzi29@gmail.com>
2021-01-21 18:36:40 +0000

    Removed all properties but value from this.state. Refactored the code using condicional rendering.

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -5,42 +5,39 @@ class PasswordInput extends React.Component {
     super(props);
     this.state = {
       value: '',
-      text: 'A password is required.',
-      icon: 'fas fa-times red'
     };
     this.handleInput = this.handleInput.bind(this);
   }
 
   handleInput(event) {
-    if (this.state.value.length > 6) {
-      this.setState({
-        text: '',
-        icon: 'fas fa-check green'
-      });
-    } else if (this.state.value.length > -1) {
-      this.setState({
-        text: 'Your password is too short.'
-      });
-    }
     this.setState({
       value: event.target.value
     });
-
   }
 
   render() {
+    let passwordLength = this.state.value.length;
+    let alert = <p className='red'>A password is required.</p>
+    let icon = <i className="fas fa-times red"></i>
+    if (passwordLength > 7) {
+      alert = <p></p>
+      icon = <i className="fas fa-check green"></i>
+    } else if (passwordLength > 0) {
+      alert = <p className='red'>Your password is too short.</p>
+    }
+
     return (
       <form>
-        <label htmlFor="password">Password:</label>
-        <div className="container">
+        <label htmlFor='password'>Password:</label>
+        <div className='container'>
           <input
-            id="password"
-            name="password"
-            type="password"
+            id='password'
+            name='password'
+            type='password'
             onChange={this.handleInput}></input>
-          <i className={this.state.icon}></i>
+          {icon}
         </div>
-        <p className="red">{this.state.text}</p>
+        {alert}
       </form>
     );
   }
