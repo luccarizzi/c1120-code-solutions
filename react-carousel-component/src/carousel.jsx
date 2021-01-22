bbd818a0f496d827d325bb0c415aeb19228c002c
Lucca Rizzi <luccarizzi29@gmail.com>
2021-01-22 15:45:11 +0000

    WIP. Setup package.json, webpack.config.js, directories, images and html/css/jsx files.

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  render() {
+    return <h1>Haya</h1>
+  }
+}
---
+++ react-carousel-component/src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Carousel from './carousel';
+
+ReactDOM.render(
+  <Carousel />,
+  document.getElementById('root')
+)
