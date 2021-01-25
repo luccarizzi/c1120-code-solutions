b261e79a4d413985ad79ea0c6287fea5288946bb
Lucca Rizzi <luccarizzi29@gmail.com>
2021-01-25 22:49:02 +0000

    Complete api-registration

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -21,20 +21,6 @@ app.post('/api/auth/sign-up', (req, res, next) => {
     throw new ClientError(400, 'username and password are required fields');
   }
 
-  /* your code starts here */
-
-  /**
-   * Hash the user's password with `argon2.hash()`
-   * Then, 😉
-   *   Insert the user's "username" and "hashedPassword" into the "users" table.
-   *   Then, 😉
-   *     Respond to the client with a 201 status code and the new user's "userId", "username", and "createdAt" timestamp.
-   *   Catch any errors.
-   * Catch any errors.
-   *
-   * Hint: Insert statements can include a `returning` clause to retrieve the insterted row(s).
-   */
-
   argon2.hash(password)
     .then(hashedPassword => {
 
