725a5a9da5dc9cf4c4ba6cdd8355df754ff6362a
Lucca Rizzi <luccarizzi29@gmail.com>
2021-01-29 16:42:27 +0000

    Left and right arrow working

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 
-function Test (props) {
-
-  console.log(props)
-
-  return (
-    <h1>{props.person.name}</h1>
-  )
-}
-
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
@@ -16,62 +7,120 @@ export default class Carousel extends React.Component {
       imageIndex: 0
     }
     this.timer = this.timer.bind(this);
+    this.leftArrow = this.leftArrow.bind(this);
+    this.rightArrow = this.rightArrow.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.renderDots = this.renderDots.bind(this);
   }
 
   componentDidMount() {
     this.timerID = setInterval(
       this.timer,
-      1500
+      2000
     )
   }
 
   timer() {
+    const { imageIndex } = this.state;
     if (this.state.imageIndex === 4) {
       this.setState({
         imageIndex: 0
       })
     } else {
       this.setState({
-        imageIndex: this.state.imageIndex + 1
+        imageIndex: imageIndex + 1
       })
     }
   }
 
-  render() {
+  leftArrow(event) {
+    const { imageIndex } = this.state;
+    if (imageIndex === 0) {
+      return this.setState({
+        imageIndex: 4
+      })
+    }
+    this.setState({
+      imageIndex: imageIndex - 1
+    })
+    clearInterval(this.timerID)
+    this.componentDidMount()
+  }
 
-    const { carousel } = this.props;
+  rightArrow(event) {
+    const { imageIndex } = this.state;
+    if (imageIndex === 4) {
+      return this.setState({
+        imageIndex: 0
+      })
+    }
+    this.setState({
+      imageIndex: imageIndex + 1
+    })
+    clearInterval(this.timerID)
+    this.componentDidMount()
+  }
+
+  renderDots(props) {
+
     const { imageIndex } = this.state;
 
+    console.log(this.props)
+
     return (
-      <Test />
+      <div className='row justify-content-center mb-4'>
+        <div className='col-4 text-center'>
+          <i className="far fa-circle px-1 circle-size"></i>
+          <i className="far fa-circle px-1 circle-size"></i>
+          <i className="far fa-circle px-1 circle-size"></i>
+          <i className="far fa-circle px-1 circle-size"></i>
+          <i className="far fa-circle px-1 circle-size"></i>
+        </div>
+      </div>
     )
+  }
 
-    // return (
-    //   <div className='container mt-4 border border-4 border-dark rounded'>
-    //     <div className='row d-flex'>
-    //       <div className='col-3 d-flex justify-content-center align-self-center'>
-    //         <i className='fas fa-chevron-left arrow-size'></i>
-    //       </div>
-    //       <div className='col-6 d-flex justify-content-center'>
-    //         <img src={carousel[imageIndex].url} />
-    //       </div>
-    //       <div className='col-3 d-flex justify-content-center align-self-center'>
-    //         <i className='fas fa-chevron-right arrow-size'></i>
-    //       </div>
-    //     </div>
+  renderPage() {
+    const { carousel } = this.props;
+    const { imageIndex } = this.state;
+    return (
+      <div className='container mt-4 border border-4 border-dark rounded'>
+        <div className='row d-flex'>
+          <div className='col-3 d-flex justify-content-center align-self-center'>
+            <a onClick={this.leftArrow}><i className='fas fa-chevron-left arrow-size'></i></a>
+          </div>
+          <div className='col-6 d-flex justify-content-center'>
+            <img src={carousel[imageIndex].url} />
+          </div>
+          <div className='col-3 d-flex justify-content-center align-self-center'>
+            <a onClick={this.rightArrow}><i className='fas fa-chevron-right arrow-size'></i></a>
+          </div>
+        </div>
 
-    //     <div className='row justify-content-center mb-4'>
-    //       <div className='col-4 text-center'>
-    //         <i className="far fa-circle px-1 circle-size"></i>
-    //         <i className="far fa-circle px-1 circle-size"></i>
-    //         <i className="far fa-circle px-1 circle-size"></i>
-    //         <i className="far fa-circle px-1 circle-size"></i>
-    //         <i className="far fa-circle px-1 circle-size"></i>
-    //       </div>
-    //     </div>
-    //   </div>
-    // )
+        <>{this.renderDots()} </>
 
+        {/* <div className='row justify-content-center mb-4'>
+          <div className='col-4 text-center'>
+            <i className="far fa-circle px-1 circle-size"></i>
+            <i className="far fa-circle px-1 circle-size"></i>
+            <i className="far fa-circle px-1 circle-size"></i>
+            <i className="far fa-circle px-1 circle-size"></i>
+            <i className="far fa-circle px-1 circle-size"></i>
+          </div>
+        </div> */}
+
+      </div>
+    )
+  }
+
+  render() {
+    const { imageIndex } = this.state;
+    console.log(imageIndex)
+    return (
+      <>
+        {this.renderPage()}
+      </>
+    )
   }
 }
 
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,19 +2,15 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Carousel from './carousel';
 
-// const images = [
-//   { id: '001', url: '../images/001.png' },
-//   { id: '002', url: '../images/004.png' },
-//   { id: '003', url: '../images/007.png' },
-//   { id: '004', url: '../images/025.png' },
-//   { id: '005', url: '../images/039.png' },
-// ]
-
-const person = {
-  name: 'Lucca'
-}
+const images = [
+  { id: '001', url: '../images/001.png' },
+  { id: '002', url: '../images/004.png' },
+  { id: '003', url: '../images/007.png' },
+  { id: '004', url: '../images/025.png' },
+  { id: '005', url: '../images/039.png' },
+]
 
 ReactDOM.render(
-  <Carousel person={person.name}/>,
+  <Carousel carousel={images} />,
   document.getElementById('root')
 )
